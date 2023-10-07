419f1f78b99c63a38a9001131712fc8654821dc2
kutukov2000 <kutukov16.2005@gmail.com>
2023-10-07 11:23:24 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,137 @@
+class Card {
+    constructor(image, title, height = '7vh', isLogo = false) {
+        this.image = image;
+        this.title = title;
+        this.height = height;
+
+        isLogo ? this.isFlipped = true : this.isFlipped = false;
+        this.isLogo = isLogo;
+    }
+
+    getCard() {
+        return `
+            <div class="card ${this.isFlipped ? 'flipped' : 'closed'}">
+                    <div id="${this.isLogo ? 'logo' : ''}">
+                        <img style="height: ${this.height};" src="${this.image}">
+                        <h3>${this.title}</h3>
+                    </div>
+            </div>`
+    }
+
+    flip() {
+        this.isFlipped = !this.isFlipped;
+    }
+}
+
+const field = document.querySelector("#field");
+
+let startTime;
+let isStart = true;
+
+let openCards = [];
+let matchedPairs = 0;
+let matchedPairToWin;
+
+
+
+function Initialization() {
+    clearField();
+
+    let cards = [];
+
+    const animalData = [
+        { image: 'images/toucan.png', title: 'Toucan', height: '9vh' },
+        { image: 'images/turtle.png', title: 'Turtle' },
+        { image: 'images/elephant.png', title: 'Elephant', height: '9vh' },
+        { image: 'images/zebra.png', title: 'Zebra', height: '11vh' },
+        { image: 'images/fox.png', title: 'Fox', height: '10vh' },
+        { image: 'images/dog.png', title: 'Dog', height: '9vh' },
+        { image: 'images/owl.png', title: 'Owl', height: '10vh' },
+        { image: 'images/penguin.png', title: 'Penguin', height: '10vh' },
+        { image: 'images/sheep.png', title: 'Sheep', height: '10vh' },
+        { image: 'images/mouse.png', title: 'Mouse' },
+        { image: 'images/rabbit.png', title: 'Rabbit', height: '11vh' },
+        { image: 'images/lion.png', title: 'Lion', height: '11vh' },
+    ];
+
+    matchedPairToWin = animalData.length;
+    console.log(matchedPairToWin);
+
+    for (const data of animalData) {
+        cards.push(new Card(data.image, data.title, data.height));
+        cards.push(new Card(data.image, data.title, data.height));
+    }
+
+
+    shuffleArray(cards);
+
+    //Insert logo card
+    cards.splice(12, 0, new Card('images/m.png', '', '12vh', true));
+
+    //Add cards to game field
+    cards.forEach((card) => field.innerHTML += card.getCard());
+}
+
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function clearField() {
+    field.innerHTML = '';
+}
+Initialization();
+
+function handleCardClick(card) {
+    if (isStart) {
+        startTime = Date.now();
+    }
+
+    if (card.classList.contains('flipped') || openCards.length >= 2) {
+        return;
+    }
+
+    isStart = false;
+
+    card.classList.remove('closed');
+    card.classList.add('flipped');
+
+    openCards.push(card);
+
+    if (openCards.length === 2) {
+        const [card1, card2] = openCards;
+        const img1 = card1.querySelector('img').src;
+        const img2 = card2.querySelector('img').src;
+
+        if (img1 === img2) {
+            openCards = [];
+            matchedPairs++;
+
+            // Check for win
+            if (matchedPairs === matchedPairToWin) {
+                alert(`You are winner. Your time ${Math.floor((Date.now() - startTime) / 1000)}s`);
+                Initialization();
+            }
+        }
+        else {
+            setTimeout(() => {
+                card1.classList.add('closed');
+                card1.classList.remove('flipped');
+                card2.classList.add('closed');
+                card2.classList.remove('flipped');
+                openCards = [];
+            }, 500);
+        }
+    }
+}
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach((card) => {
+    card.addEventListener('click', () => {
+        handleCardClick(card);
+    });
+});
